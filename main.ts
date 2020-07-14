ed3a4f4ddf3165cbc7a9dbeea23771002b542a48
maxoumax <44801188+maxoumax@users.noreply.github.com>
2020-07-14 19:06:05 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 input.onButtonPressed(Button.A, function () {
     led.unplot(led2, 0)
+    led.unplot(led2, 1)
+    led.unplot(led2, 2)
+    led.unplot(led2, 3)
+    led.unplot(led2, 4)
     led2 += -1
 })
 input.onButtonPressed(Button.B, function () {
     led.unplot(led2, 0)
+    led.unplot(led2, 1)
+    led.unplot(led2, 2)
+    led.unplot(led2, 3)
+    led.unplot(led2, 4)
     led2 += 1
-    if (led2 < 0) {
-        led2 = 0
-    }
 })
 let led2 = 0
 led2 = 0
 basic.forever(function () {
-    led.plot(Math.constrain(led2, 0, 4), 0)
+    if (led2 == 0) {
+        led.plot(led2, 0)
+    } else if (led2 == 0) {
+        led.plot(led2, 1)
+    } else if (led2 == 0) {
+        led.plot(led2, 2)
+    } else if (led2 == 0) {
+        led.plot(led2, 3)
+    } else {
+        led.plot(led2, 4)
+    }
 })
